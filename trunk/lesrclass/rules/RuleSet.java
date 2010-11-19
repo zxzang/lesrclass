a19de38541e4607b57132035536409a55b5be63c
johnwhurley <johnwhurley@6d846559-18e7-9ee4-f435-0af34f6fda55>
2010-11-19 16:17:50 +0000

    update to current work status.  About to change calcs so that variables vary around zero instead of 1

--- trunk/lesrclass/rules/RuleSet.java
+++ trunk/lesrclass/rules/RuleSet.java
@@ -8,9 +8,7 @@ public interface RuleSet {
 
 	public abstract void showRules();
 
-	public abstract Rule.RecType getRecommendation(Double[] SMAsIn,
-			String lastTick);
-
+	
 	Rule.RecType getRecommendation(double[] inputs, double[] outputs);
 
 	
--- trunk/lesrclass/rules/RuleSetBuyHold.java
+++ trunk/lesrclass/rules/RuleSetBuyHold.java
@@ -12,12 +12,6 @@ public class RuleSetBuyHold implements RuleSet {
 	}
 
 	@Override
-	public RecType getRecommendation(Double[] SMAsIn, String lastTick) {
-		// TODO Auto-generated method stub
-		return RecType.LONG;
-	}
-
-	@Override
 	public RecType getRecommendation(double[] inputs, double[] outputs) {
 		// TODO Auto-generated method stub
 		return RecType.LONG;
